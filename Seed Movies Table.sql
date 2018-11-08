277f7d0a25ec9a3975b840d06a601191bbe93773
imkwaq <imikhail.waq@gmail.com>
2018-11-08 18:06:44 +0300

    Addwd rewrited sql scripts that allowed to seed the tables.

--- Seed Movies Table.sql
+++ Seed Movies Table.sql
@@ -1,12 +1,12 @@
-﻿SET IDENTITY_INSERT [dbo].[Movies] ON
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (1, N'Nineteen Eighty-Four (1984)', N'Michael Radford', N'1984-10-10 00:00:00', 110, 22)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (2, N'One Flew Over the Cuckoo''s Nest', N'Miloš Forman', N'1975-11-19 00:00:00', 133, 5)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (3, N'Harry Potter and the Philosopher''s Stone', N'	Chris Columbus', N'2001-11-4 00:00:00', 152, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (4, N'Harry Potter and the Chamber of Secrets', N'Chris Columbus', N'2002-11-3 00:00:00', 161, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (5, N'Harry Potter and the Prisoner of Azkaban', N'Alfonso Cuarón', N'2004-05-23 00:00:00', 142, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (6, N'Harry Potter and the Goblet of Fire', N'Mike Newell', N'2005-11-6 00:00:00', 157, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (7, N'Harry Potter and the Order of the Phoenix', N'David Yates', N'2007-07-28 00:00:00', 138, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (8, N'Harry Potter and the Half-Blood Prince', N'David Yates', N'2009-07-7 00:00:00', 153, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (9, N'Harry Potter and the Deathly Hallows – Part 1', N'David Yates', N'2010-11-11 00:00:00', 146, 25)
-INSERT INTO [dbo].[Movies] ([Id], [Name], [Director], [ReleaseDate], [RunningTime], [Genre_Id]) VALUES (10, N'Harry Potter and the Deathly Hallows – Part 2 ', N'David Yates', N'2011-07-11 00:00:00', 130, 25)
-SET IDENTITY_INSERT [dbo].[Movies] OFF
+﻿SET IDENTITY_INSERT [dbo].[MovieModels] ON
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (1, N'Nineteen Eighty-Four (1984)', N'Michael Radford', N'1984-10-10 00:00:00', 110, 22)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (2, N'One Flew Over the Cuckoo''s Nest', N'Miloš Forman', N'1975-11-19 00:00:00', 133, 5)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (3, N'Harry Potter and the Philosopher''s Stone', N'	Chris Columbus', N'2001-11-4 00:00:00', 152, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (4, N'Harry Potter and the Chamber of Secrets', N'Chris Columbus', N'2002-11-3 00:00:00', 161, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (5, N'Harry Potter and the Prisoner of Azkaban', N'Alfonso Cuarón', N'2004-05-23 00:00:00', 142, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (6, N'Harry Potter and the Goblet of Fire', N'Mike Newell', N'2005-11-6 00:00:00', 157, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (7, N'Harry Potter and the Order of the Phoenix', N'David Yates', N'2007-07-28 00:00:00', 138, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (8, N'Harry Potter and the Half-Blood Prince', N'David Yates', N'2009-07-7 00:00:00', 153, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (9, N'Harry Potter and the Deathly Hallows – Part 1', N'David Yates', N'2010-11-11 00:00:00', 146, 25)
+INSERT INTO [dbo].[MovieModels] ([Id], [Name], [Director], [ReleaseDate], [RunningTime],  [MovieGenreModels_Id]) VALUES (10, N'Harry Potter and the Deathly Hallows – Part 2 ', N'David Yates', N'2011-07-11 00:00:00', 130, 25)
+SET IDENTITY_INSERT [dbo].[MovieModels] OFF
