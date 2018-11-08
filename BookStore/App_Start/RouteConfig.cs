123ef860364fc89fdf02a1eb7fd5ab4c61640ce7
imkwaq <imikhail.waq@gmail.com>
2018-11-08 20:02:50 +0300

    Fixed main layout ActionLinks and added two new routes.

--- BookStore/App_Start/RouteConfig.cs
+++ BookStore/App_Start/RouteConfig.cs
@@ -18,6 +18,18 @@ namespace BookStore
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+               name: "MovieRoute",
+               url: "Movies/{action}/{id}",
+               defaults: new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+                name: "BookRoute",
+                url: "Books/{action}/{id}",
+                defaults: new { controller = "Books", action = "Index", id = UrlParameter.Optional }
+            );
+            
+           
         }
     }
 }
--- BookStore/Models/BookModels.cs
+++ BookStore/Models/BookModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,23 @@ namespace BookStore.Models
     public class BookModels
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "Release Year")]
         public short ReleaseYear { get; set; }
+
+        [Display(Name = "Genre")]
         public BookGenreModels BookGenreModels { get; set; }
+
         public string Description { get; set; }
+
         public decimal Price { get; set; }
+
+        [Display(Name = "Related movie")]
         public MovieModels MovieModels { get; set; }
     }
 }
