be1802f1b56226c4e2854b2a1db89a70552f614e
imkwaq <imikhail.waq@gmail.com>
2018-11-08 17:32:37 +0300

    Seeded books table

---
+++ Seed Books Table.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Books] ON
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (1, N'Harry Potter and the Philosopher''s Stone', 'J.K.Rowling', null, 215, 23, 3)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (2, N'Harry Potter and the Chamber of Secrets', 'J.K.Rowling', null, 215, 23, 4)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (3, N'Harry Potter and the Prisoner of Azkaban', 'J.K.Rowling', null, 215, 23, 5)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (4, N'Harry Potter and the Goblet of Fire', 'J.K.Rowling', null, 215, 23, 6)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (5, N'Harry Potter and the Order of the Phoenix', 'J.K.Rowling', null, 215, 23, 7)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (6, N'Harry Potter and the Half-Blood Prince', 'J.K.Rowling', null, 215, 23, 8)
+INSERT INTO [dbo].[Books] ([Id], [Name], [Author], [Description], [Price], [Genre_Id], [RelatedMovie_Id]) VALUES (7, N'Harry Potter and the Deathly Hallows', 'J.K.Rowling', null, 215, 23, 9)
+SET IDENTITY_INSERT [dbo].[Books] OFF
